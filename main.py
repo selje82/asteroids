ddcc3bbcaf39b3007487891ecf6139ad8c7e80a3
Glenn Seljeseth <selje82@hotmail.com>
2026-07-11 15:54:57 +0200

    Added rotation to the player character

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, PLAYER_RADIUS, LINE_WIDTH
 from logger import log_state
+from player import Player
+
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0.0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
     
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return 
+
+        Player.update(player, dt)
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        
+
         
         
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
